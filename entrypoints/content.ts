e87dca0f17e6877d8b2a2098af6618013fd29f42
kogeletey <kg@re128.org>
2025-03-21 21:55:32 +0300

    init: starting development extenstion

---
+++ entrypoints/content.ts
@@ -0,0 +1,8 @@
+import { defineContentScript } from "wxt/sandbox";
+
+export default defineContentScript({
+    matches: ['*://*.google.com/*'],
+    main() {
+        console.log('Hello content.');
+    },
+});
