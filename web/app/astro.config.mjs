91bc0405fbd8ce3e8fdaf01f5c41532f422459d8
kogeletey <kg@re128.org>
2025-04-10 22:33:17 +0300

    feat: integrate a tron link connect hooks

--- web/app/astro.config.mjs
+++ web/app/astro.config.mjs
@@ -1,9 +1,10 @@
 import sitemap from "@astrojs/sitemap"
+import vue from "@astrojs/vue"
 import lit from "@semantic-ui/astro-lit"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
     output: "static",
-    integrations: [sitemap(), lit()],
+    integrations: [sitemap(), lit(), vue()],
 })
---
+++ web/app/src/components/tron-link/tron-link.vue
@@ -0,0 +1,41 @@
+<template>
+  <WalletProvider
+    :adapters="adapters"
+    @connect="onConnect"
+    @disconnect="onDisconnect"
+  >
+    <WalletModalProvider>
+      <WalletActionButton> </WalletActionButton>
+    </WalletModalProvider>
+  </WalletProvider>
+</template>
+<script setup lang="ts">
+import { WalletProvider } from "@tronweb3/tronwallet-adapter-vue-hooks";
+import {
+  TronLinkAdapter,
+  // WalletConnectAdapter,
+} from "@tronweb3/tronwallet-adapters";
+import {
+  WalletModalProvider,
+  WalletActionButton,
+} from "@tronweb3/tronwallet-adapter-vue-ui";
+import "./tron-link.css";
+
+const tronLink = new TronLinkAdapter();
+const adapters = [tronLink];
+
+function onConnect(address: string) {
+  console.log("[wallet hooks] onConnect: ", address);
+}
+
+function onDisconnect() {
+  console.log("[wallet hooks] onDisconnect");
+}
+</script>
+
+<style scoped>
+:deep(.adapter-vue-button) {
+  background-color: var(--background);
+  color: var(--foreground);
+}
+</style>
