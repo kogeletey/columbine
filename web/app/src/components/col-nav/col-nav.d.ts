8937aa866b16243d525a91e2e649212ade22916c
kogeletey <kg@re128.org>
2025-04-11 23:13:31 +0300

    feat: block with watch contract

---
+++ web/app/src/components/col-nav/col-nav.d.ts
@@ -0,0 +1,4 @@
+export type ColNavStateProps = {
+    isView?: boolean
+    changeView?: () => boolean
+}
--- web/app/src/components/col-nav/col-nav.ts
+++ web/app/src/components/col-nav/col-nav.ts
@@ -1,18 +1,28 @@
 import { LitElement, unsafeCSS } from "lit"
 import { customElement, property } from "lit/decorators.js"
-// import colNav from "./col-nav.css?inline"
-import { ColNavTemplate } from "./col-nav.html.ts"
+import colNavStyles from "./col-nav.css?inline"
+import ColNavTemplate  from "./col-nav.html.ts"
 
 @customElement("col-nav")
 export class ColNav extends LitElement {
-    // static styles = unsafeCSS(colNavStyles)
-    render() {
-        return ColNavTemplate()
-    }
+  static styles = unsafeCSS(colNavStyles)
+  @property({ type: Boolean })
+  isView: boolean = false
+
+  public changeView = () => {
+    this.isView = !this.isView
+  }
+
+  render() {
+    return ColNavTemplate({
+      changeView: this.changeView,
+      isView: this.isView
+    })
+  }
 }
 
 declare global {
-    interface HTMLElementTagNameMap {
-        "col-nav": ColNav
-    }
+  interface HTMLElementTagNameMap {
+    "col-nav": ColNav
+  }
 }
