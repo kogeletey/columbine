959b5c80a71d19c56d8895f69535954e18792ac4
kogeletey <kg@re128.org>
2025-05-25 21:57:31 +0300

    feat: migrate styles and support core goals types and default settings

--- client/src/components/col-nav/col-nav.html.ts
+++ client/src/components/col-nav/col-nav.html.ts
@@ -1,20 +1,11 @@
 import type { ColNavStateProps } from "./col-nav.d.ts"
 
 import { html } from "lit"
-import { unsafeSVG } from "lit/directives/unsafe-svg.js"
-import carbonViewOff from "./carbon-view-off.svg?raw"
-import carbonView from "./carbon-view.svg?raw"
+// import { unsafeSVG } from "lit/directives/unsafe-svg.js"
 
-export default function ColNavTemplate(state: ColNavStateProps) {
-  return html`
+export default function ColNavTemplate(_state: ColNavStateProps) {
+    return html`
         <nav>
-            <button class="watch" @click="${state.changeView}" >
-                    ${state.isView
-                        ? unsafeSVG(carbonViewOff)
-                        : unsafeSVG(carbonView)
-                    }
-    </button>
-    <slot name = "account"> </slot>
-    </nav>
+        </nav>
         `
 }
--- client/src/components/super-input/super-input.block.ts
+++ client/src/components/super-input/super-input.block.ts
@@ -1,66 +1,67 @@
 import type { API } from "@editorjs/editorjs"
 import type {
-  BlockToolConstructorOptions,
+    BlockToolConstructorOptions,
 } from "@editorjs/editorjs/types/tools"
 
 import "@/components/super-input/filter-buttons"
+import "./super-input.css";
 
 export type SuperInputParams = BlockToolConstructorOptions
 
 export class SuperInputBlock {
-  private wrapper: HTMLElement | null
-  private api: API
+    private wrapper: HTMLElement | null
+    private api: API
 
-  constructor({ api }: SuperInputParams) {
-    this.wrapper = null
-    this.api = api
-  }
-
-  static get toolbox() {
-    return {
-      title: "SuperInput",
+    constructor({ api }: SuperInputParams) {
+        this.wrapper = null
+        this.api = api
     }
-  }
 
-  static get isReadOnlySupported() {
-    return true
-  }
-
-  static get enableLineBreaks() {
-    return true
-  }
-
-  getSearch(search: HTMLInputElement | null): void {
-    if (!search) {
-      return
+    static get toolbox() {
+        return {
+            title: "SuperInput",
+        }
     }
-    const value = search.value
-    this.api.blocks.insert("tradingView", { search: value })
-  }
 
-  render() {
-    this.wrapper = document.createElement("fieldset")
-    this.wrapper.classList.add("super")
-
-    const input = document.createElement("input")
-    const filterButtons = document.createElement("filter-buttons")
-
-    this.wrapper.appendChild(input)
-    this.wrapper.appendChild(filterButtons)
-    input.placeholder = "Search smart contract"
-
-    this.wrapper.addEventListener("click", () => {
-      const input = document.querySelector("input")
-      this.getSearch(input)
-    })
-    return this.wrapper
-  }
-
-  save(blockContent) {
-    const input = blockContent.querySelector("input")
-
-    return {
-      search: input.value,
+    static get isReadOnlySupported() {
+        return true
     }
-  }
+
+    static get enableLineBreaks() {
+        return true
+    }
+
+    getSearch(search: HTMLInputElement | null): void {
+        if (!search) {
+            return
+        }
+        const value = search.value
+        this.api.blocks.insert("tradingView", { search: value })
+    }
+
+    render() {
+        this.wrapper = document.createElement("fieldset")
+        this.wrapper.classList.add("super")
+
+        const input = document.createElement("input")
+        const filterButtons = document.createElement("filter-buttons")
+
+        this.wrapper.appendChild(input)
+        this.wrapper.appendChild(filterButtons)
+        input.placeholder = "Search"
+
+        this.wrapper.addEventListener("click", () => {
+            const input = document.querySelector("input")
+            this.getSearch(input)
+        })
+        return this.wrapper
+    }
+
+    save(blockContent) {
+        const input = blockContent.querySelector("input")
+
+        return {
+            search: input.value,
+        }
+    }
 }
